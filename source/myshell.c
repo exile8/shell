c08c910d8eb8904708c5661c487e1843376d493f
Yuliya Park <pjulie071719@gmail.com>
2021-10-20 03:02:54 +0600

    Изменена структура программы
    Ввод осуществляется с помощью трех функций: get_word, get_arg и get_list. Добавлена функция prepare_list. Функции redirect принимают не имена файлов, а их дескрипторы.

--- source/myshell.c
+++ source/myshell.c
@@ -12,11 +12,9 @@ char *get_word(char *end) {
     char symbol;
     int len = 0;
     symbol = getchar();
-    /* Пропускаем лишние пробелы и табуляции */
     while (symbol == ' ' || symbol == '\t') {
         symbol = getchar();
     }
-    /* Считываем слово до следующего разделительного символа */
     while (symbol != ' ' && symbol != '\t' && symbol != '\n') {
         word_ptr = realloc(word_ptr, (len + 1) * sizeof(char));
         word_ptr[len] = symbol;
@@ -29,251 +27,142 @@ char *get_word(char *end) {
     return word_ptr;
 }
 
-char **get_list() {
-    char **list = NULL;
+char **get_args(char *end_fl) {
+    char **arg_ptr = NULL;
     char last_symb = '\0';
     int len = 0;
-    /* Считываем строку до переноса, добавляя слова в динамический массив */
-    while (last_symb != '\n' && last_symb != '|') {
-        list = realloc(list, (len + 1) * sizeof(char *));
-        list[len] = get_word(&last_symb);
-        if (!strcmp(list[len], "<") && last_symb != '\n') {
-            free(list[len]);
-            *input_stream = get_word(&last_symb);
-            continue;
-        }
-        if (!strcmp(list[len], ">") && last_symb != '\n') {
-            free(list[len]);
-            *output_stream = get_word(&last_symb);
-            continue;
+    while (last_symb != '\n') {
+        arg_ptr = realloc(arg_ptr, (len + 1) * sizeof(char *));
+        arg_ptr[len] = get_word(&last_symb);
+        if (!strcmp(arg_ptr[len], "|")) {
+            break;
         }
         len++;
     }
-    /* Последняя строка в массиве - пустая */
-    if (last_symb == '|') {
-        *pipe_flag = 1;
-        len--;
-        free(list[len]);
-        list[len] = NULL;
-        return list;
+    if (last_symb != '\n') {
+        free(arg_ptr[len]);
+        arg_ptr[len] = NULL;
+        return arg_ptr;
     }
-    list = realloc(list, (len + 1) * sizeof(char *));
+    arg_ptr = realloc(arg_ptr, (len + 1) * sizeof (char *));
+    *end_fl = 1;
+    arg_ptr[len] = NULL;
+    return arg_ptr;
+}
+
+char ***get_list(int *pipe_num) {
+    char ***list = NULL;
+    char end_flag = 0;
+    int len = 0;
+    while (end_flag != 1) {
+        list = realloc(list, (len + 1) * sizeof(char **));
+        list[len] = get_args(&end_flag);
+        len++;
+    }
+    *pipe_num = len - 1;
+    list = realloc(list, (len + 1) * sizeof(char **));
     list[len] = NULL;
     return list;
 }
 
-char **remove_list(char **list) {
-    int index = 0;
-    while (list[index] != NULL) {
-        free(list[index]);
-        index++;
+char ***remove_list(char ***list) {
+    for (int i = 0; list[i]; i++) {
+        for (int j = 0; list[i][j]; j++) {
+            free(list[i][j]);
+        }
+        free(list[i]);
     }
     free(list);
     return NULL;
 }
 
-int redirect_input(char *input_stream) {
-    if (input_stream == NULL) {
-        return 0;
+char ***prepare_list(char ***list, int *input_fd, int *output_fd) {
+    for (int i = 0; list[i]; i++) {
+        for (int j = 1; list[i][j]; j++) {
+            if (list[i][j - 1] != NULL && !strcmp(list[i][j - 1], "<")) {
+                *input_fd = open(list[i][j], O_RDONLY);
+                free(list[i][j - 1]);
+                list[i][j - 1] = NULL;
+                free(list[i][j]);
+                list[i][j] = NULL;
+            }
+            if (list[i][j - 1] != NULL && !strcmp(list[i][j - 1], ">")) {
+                *output_fd = open(list[i][j], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+                free(list[i][j - 1]);
+                list[i][j - 1] = NULL;
+                free(list[i][j]);
+                list[i][j] = NULL;
+            }
+        }
     }
-    int fd = open(input_stream, O_RDONLY);
-    if (fd < 0) {
+    if (input_fd < 0 || output_fd < 0) {
         perror("open");
-        free(input_stream);
-        return 1;
+        list = remove_list(list);
+    }
+    return list;
+}
+
+int redirect_input(int fd) {
+    if (fd == STDIN_FILENO) {
+        return 0;
     }
     if (dup2(fd, STDIN_FILENO) < 0) {
         perror("dup2");
-        free(input_stream);
         return 1;
     }
     if (close(fd) < 0) {
         perror("close");
-        free(input_stream);
         return 1;
     }
-    free(input_stream);
     return 0;
 }
 
-int redirect_output(char *output_stream) {
-    if (output_stream == NULL) {
+int redirect_output(int fd) {
+    if (fd == STDOUT_FILENO) {
         return 0;
-    }
-    int fd = open(output_stream, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-    if (fd < 0) {
-        perror("open");
-        free(output_stream);
-        return 1;
     }
     if (dup2(fd, STDOUT_FILENO) < 0) {
         perror("dup2");
-        free(output_stream);
         return 1;
     }
     if (close(fd) < 0) {
         perror("close");
-        free(output_stream);
-        return 1;
-    }
-    free(output_stream);
-    return 0;
-}
-
-int execute(char **command, char *input_stream, char *output_stream) {
-    pid_t pid;
-    pid = fork();
-    if (pid < 0) {
-        perror("fork");
-        return 1;
-    }
-    if (pid == 0) {
-        if (redirect_input(input_stream) > 0) {
-            return 1;
-        }
-        if (redirect_output(output_stream) > 0) {
-            return 1;
-        }
-        execvp(command[0], command);
-        /* Ошибка exec */
-        perror("exec");
-        return 1;
-    }
-    if (wait(NULL) < 0) {
-        perror("wait");
         return 1;
     }
     return 0;
 }
 
-int execute_pipe(char **arg1, char **arg2, char *input_stream, char *output_stream) {
+/* int execute(char ***cmd, int input_fd, int output_fd, int pipe_num) {
+    int fd[pipe_num][2];
+    int index;
     pid_t pid;
-    int fd[2];
-    if (pipe(fd) < 0) {
-        perror("pipe");
-        return 1;
-    }
-    pid = fork();
-    if (pid < 0) {
-        perror("fork");
-        return 1;
-    }
-    if (pid == 0) {
-        if (redirect_input(input_stream) > 0) {
-            return 1;
-        }
-        if (dup2(fd[1], STDOUT_FILENO) < 0) {
-            perror("dup2");
-            return 1;
-        }
-        if (close(fd[0]) < 0) {
-            perror("close");
-            return 1;
-        }
-        if (close(fd[1]) < 0) {
-            perror("close");
-            return 1;
-        }
-        execvp(arg1[0], arg1);
-        perror("exec");
-        return 1;
-    }
-    pid = fork();
-    if (pid < 0) {
-        perror("fork");
-        return 1;
-    }
-    if (pid == 0) {
-        if (redirect_output(output_stream) > 0) {
-            return 1;
-        }
-        if (dup2(fd[0], STDIN_FILENO) < 0) {
-            perror("dup2");
-            return 1;
-        }
-        if (close(fd[0]) < 0) {
-            perror("close");
-            return 1;
-        }
-        if (close(fd[1]) < 0) {
-            perror("close");
-            return 1;
-        }
-        execvp(arg2[0], arg2);
-        perror("exec");
-        return 1;
-    }
-    if (close(fd[0]) < 0) {
-        perror("close");
-        return 1;
-    }
-    if (close(fd[1]) < 0) {
-        perror("close");
-        return 1;
-    }
-    if (wait(NULL) < 0) {
-        perror("wait");
-        return 1;
-    }
-    if (wait(NULL) < 0) {
-        perror("wait");
-        return 1;
-    }
-    return 0;
-}
+
+    if (pipe_num = 0)
+} */
 
 int main() {
-    char *input = NULL, *output = NULL;
-    int pipe_flag = 0;
-    char **command = get_list(&pipe_flag, &input, &output);
-    char **command2 = NULL;
+    int input_fd = STDIN_FILENO, output_fd = STDOUT_FILENO, pipes = 0;
+    char ***command = get_list(&pipes);
+    command = prepare_list(command, &input_fd, &output_fd);
+    if (command == NULL) {
+        exit(1);
+    }
     while (1) {
-        if (!strcmp(command[0], "exit") || !strcmp(command[0], "quit")) {
-            if (input != NULL) {
-                free(input);
-            }
-            if (output != NULL) {
-                free(output);
-            }
+        if (!strcmp(command[0][0], "exit") || !strcmp(command[0][0], "quit")) {
             command = remove_list(command);
             return 0;
         }
-        if (pipe_flag == 1) {
-            command2 = get_list(&pipe_flag, &input, &output);
-            if (execute_pipe(command, command2, input, output) > 0) {
-                command = remove_list(command);
-                command2 = remove_list(command2);
-                if (input != NULL) {
-                    free(input);
-                }
-                if (output != NULL) {
-                    free(output);
-                }
-                exit(1);
-            }
-        } else if (execute(command, input, output) > 0) {
+        if (execute(command, input_fd, output_fd, pipes) > 0) {
             command = remove_list(command);
-            if (input != NULL) {
-                free(input);
-            }
-            if (output != NULL) {
-                free(output);
-            }
             exit(1);
         }
         command = remove_list(command);
-        if (command2 != NULL) {
-            command2 = remove_list(command2);
+        input_fd = STDIN_FILENO, output_fd = STDOUT_FILENO;
+        pipes = 0;
+        command = get_list(&pipes);
+        command = prepare_list(command, &input_fd, &output_fd);
+        if (command == NULL) {
+            exit(1);
         }
-        if (input != NULL) {
-            free(input);
-            input = NULL;
-        }
-        if (output != NULL) {
-            free(output);
-            output = NULL;
-        }
-        pipe_flag = 0;
-        command = get_list(&pipe_flag, &input, &output);
     }
 }
